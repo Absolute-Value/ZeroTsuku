c695e28e6e8ebb3712bb0228e0496953c2cf3d8c
Absolute-Value <ji.kei1220@gmail.com>
2022-09-15 20:57:06 +0900

    Changed gitignore

--- common/layers.py
+++ common/layers.py
@@ -55,21 +55,29 @@ class Sigmoid:
         dx = dout * (1.0 - self.out) * self.out
         return dx
 
-def softmax(a):
-    c = np.max(a)
-    exp_a = np.exp(a - c) # オーバーフロー対策
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
+def softmax(x):
+    if x.ndim == 2:
+        x = x - x.max(axis=1, keepdims=True)
+        x = np.exp(x)
+        x /= x.sum(axis=1, keepdims=True)
+    elif x.ndim == 1:
+        x = x - np.max(x)
+        x = np.exp(x) / np.sum(np.exp(x))
 
-    return y
+    return x
 
 def cross_entropy_error(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
-
+        
+    # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
     batch_size = y.shape[0]
-    return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
 class SoftmaxWithLoss:
     def __init__(self) -> None:
